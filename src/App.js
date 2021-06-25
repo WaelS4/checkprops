2b8771c879fc42db8063530cafd1b8d3e9b3d418
WaelS4 <wael.sellami.44@gmail.com>
2021-06-25 02:49:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Profile from './profile/Profile';
+import Photo from './profile/Photo';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
+  const user = [
+    {
+      fullName: "Wael Sellami",
+      bio: "étudiant GoMyCode 2021",
+      profession: "Full Stack JS",
+    },
+
+  ];
+  const handleName = (fullName) => {
+    alert(`Profile ${fullName}`)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: "#B2DCFF", height: "700px" }}>
+      <Photo>./img/photoprofile.jpg</Photo>
+      {user.map((e, index) => <Profile key={index} handleName={handleName} bio={e.bio} fullName={e.fullName} profession={e.profession} >
+      </Profile>
+      )
+      }
+
     </div>
   );
 }
